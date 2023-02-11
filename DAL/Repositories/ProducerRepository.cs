fd5094e40ae91c9edbd7ffba57aeab293faeb697
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-11 09:38:43 +0200

    Add ProducerRepository

---
+++ DAL/Repositories/ProducerRepository.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Context;
+using DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class ProducerRepository
+    {
+        private FarmacyContext _context;
+        public ProducerRepository(FarmacyContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Producer> GetProducers() => _context.Producers.ToList();
+
+        public Producer GetProducer(int id) => _context.Producers.Find(id);
+
+        public void AddUpdateProducer(Producer producer)
+        {
+            _context.Producers.AddOrUpdate(producer);
+            _context.SaveChanges();
+        }
+
+        public void RemoveProducer(Producer producer)
+        {
+            _context.Producers.Remove(producer);
+            _context.SaveChanges();
+        }
+    }
+}
