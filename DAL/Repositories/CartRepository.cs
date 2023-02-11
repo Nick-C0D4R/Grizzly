092a9891583d264dbe210e16cbcf46061e3cced0
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-11 09:56:11 +0200

    Add CartRepository

---
+++ DAL/Repositories/CartRepository.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Context;
+using DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class CartRepository
+    {
+        private FarmacyContext _context;
+
+        public CartRepository(FarmacyContext context) => _context = context;
+
+        public IEnumerable<Cart> GetCarts() => _context.Carts.ToList();
+
+        public Cart GetCart(int id) => _context.Carts.Find(id);
+
+        public void AddUpdateCart(Cart cart)
+        {
+            _context.Carts.AddOrUpdate(cart);
+            _context.SaveChanges();
+        }
+
+        public void RemoveUpdateCart(Cart cart)
+        {
+            _context.Carts.Remove(cart);
+            _context.SaveChanges();
+        }
+    }
+}
