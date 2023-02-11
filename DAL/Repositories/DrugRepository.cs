d69524976f1eff1136d61ac80b1c88b027301b0b
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-11 09:52:54 +0200

    Add DrugRepository

---
+++ DAL/Repositories/DrugRepository.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Context;
+using DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class DrugRepository
+    {
+        private FarmacyContext _context;
+
+        public DrugRepository(FarmacyContext context) => _context = context;
+
+        public IEnumerable<Drug> GetDrugs() => _context.Drugs.ToList();
+
+        public Drug GetDrug(int id) => _context.Drugs.Find(id);
+
+        public void AddUpdateDrug(Drug drug)
+        {
+            _context.Drugs.AddOrUpdate(drug);
+            _context.SaveChanges();
+        }
+
+        public void RemoveDrug(Drug drug)
+        {
+            _context.Drugs.Remove(drug);
+            _context.SaveChanges();
+        }
+    }
+}
