93213df613b30a980d74d31e9646cd28f9d72852
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-11 09:30:03 +0200

    Update RoleRepository in DAL

--- DAL/Repositories/RoleRepository.cs
+++ DAL/Repositories/RoleRepository.cs
@@ -2,6 +2,7 @@
 using DAL.Tables;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +36,22 @@ namespace DAL.Repositories
                 Id = 4,
                 RoleName = "Admin"
             });
+        }
+
+        public IEnumerable<Role> GetRoles() => _context.Roles.ToList();
+
+        public Role GetRole(int id) => _context.Roles.Find(id);
+
+        public void AddUpdateRole(Role role)
+        {
+            _context.Roles.AddOrUpdate(role);
+            _context.SaveChanges();
+        }
+
+        public void RemoveRole(Role role)
+        {
+            _context.Roles.Remove(role);
+            _context.SaveChanges();
         }
     }
 }
