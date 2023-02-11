89edba7cac3fbefeecdeaadcce22a835f0717af2
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-11 09:32:05 +0200

    Add FarmacyOfficeRepository

--- DAL/Repositories/FarmacyOfficeRepository.cs
+++ DAL/Repositories/FarmacyOfficeRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DAL.Context;
+using DAL.Tables;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,22 @@ namespace DAL.Repositories
         public FarmacyOfficeRepository(FarmacyContext context)
         {
             _context = context;
+        }
+
+        public IEnumerable<FarmacyOffice> GetOffices() => _context.FarmacyOffices.ToList();
+
+        public FarmacyOffice GetOffice(int id) => _context.FarmacyOffices.Find(id);
+
+        public void AddUpdateFarmacy(FarmacyOffice farmacy)
+        {
+            _context.FarmacyOffices.AddOrUpdate(farmacy);
+            _context.SaveChanges();
+        }
+
+        public void RemoveFarmacy(FarmacyOffice farmacy)
+        {
+            _context.FarmacyOffices.Remove(farmacy);
+            _context.SaveChanges();
         }
     }
 }
