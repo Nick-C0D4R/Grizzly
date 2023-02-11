f4c4da3b34acf69152f994a78a13e24b9fec526e
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-11 09:59:36 +0200

    Add OrderRepository

---
+++ DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Context;
+using DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class OrderRepository
+    {
+        private FarmacyContext _context;
+
+        public OrderRepository(FarmacyContext context) => _context = context;
+
+        public IEnumerable<Order> GetOrders() => _context.Orders.ToList();
+
+        public Order GetOrder(int id) => _context.Orders.Find(id);
+
+        public void AddUpdateOrder(Order order)
+        {
+            _context.Orders.AddOrUpdate(order);
+            _context.SaveChanges();
+        }
+
+        public void RemoveOrder(Order order)
+        {
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
+        }
+    }
+}
