cc428d38e01a1b7dc5b9f765d49e8aef6d18f6d8
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-11 09:45:10 +0200

    Add UserRepository

---
+++ DAL/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Context;
+using DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class UserRepository
+    {
+        private FarmacyContext _context;
+
+        public UserRepository(FarmacyContext context) => _context = context;
+
+        public IEnumerable<User> GetUsers() => _context.Users.ToList();
+
+        public User GetUser(int id) => _context.Users.Find(id);
+
+        public void AddUpdateUser(User user)
+        {
+            _context.Users.AddOrUpdate(user);
+            _context.SaveChanges();
+        }
+
+        public void RemoveUser(User user)
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+    }
+}
