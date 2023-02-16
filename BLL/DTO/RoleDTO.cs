7fef41ab4a43f88a4ffe7fefedb6fe7cdc6b7b45
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-16 16:31:00 +0200

    Add DTO; Add RoleDTO

---
+++ BLL/DTO/RoleDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    //DTO - Data Transfer Object
+    public class RoleDTO
+    {
+        public string RoleName { get; set; }
+    }
+}
