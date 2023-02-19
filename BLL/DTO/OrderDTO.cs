08746ba5c2d8d62422be78fb918ebddb7baf7f71
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-19 14:32:11 +0200

    Add OrderDTO

---
+++ BLL/DTO/OrderDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public int OfficeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CartId { get; set; }
+
+        public override string ToString()
+        {
+            return $"Office id: {OfficeId} | Cart id: {CartId} | Ordered: {OrderDate}";
+        }
+    }
+}
