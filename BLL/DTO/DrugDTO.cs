ed227bd0d5d519d9c234650ca0b6de7f39e8dce8
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-19 14:39:58 +0200

    Add DrugDTO

---
+++ BLL/DTO/DrugDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class DrugDTO
+    {
+        public int Id { get; set; }
+        public string DrugName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int ProducerId { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drug name: {DrugName} | Description: {Description} | Quantity: {Quantity} | " +
+                $"Price: {Price} | Producer id: {ProducerId}";
+        }
+
+    }
+}
