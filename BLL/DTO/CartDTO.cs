c4016e126431ae1d920cb268f36b18d6ca4dcb9b
Mykola Kovshar <knikolay20031230@gmail.com>
2023-02-20 13:17:29 +0200

    Add CartDTO

---
+++ BLL/DTO/CartDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class CartDTO
+    {
+        public int Id { get; set; }
+
+        public string UserLogin { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} | User login: {UserLogin} | Order date: {OrderDate}";
+        }
+    }
+}
