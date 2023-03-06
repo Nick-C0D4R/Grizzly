0bb9785f25c892862c08effab5adfdf850f79118
Mykola Kovshar <knikolay20031230@gmail.com>
2023-03-06 17:24:07 +0200

    Rebase IContextTable and IContextRepository into Interfaces directory

---
+++ DAL/Interfaces/IContextRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IContextRepository<T> where T : IContextTable
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void AddUpdate(T entity);
+
+        void Remove(T entity);
+    }
+}
