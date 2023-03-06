9543e1fbd2f32a06bc2889bea4d7e45b20a6698f
Mykola Kovshar <knikolay20031230@gmail.com>
2023-03-06 17:25:04 +0200

    Update Tables

--- DAL/Tables/DrugType.cs
+++ DAL/Tables/DrugType.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using DAL.Interfaces;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Tables
 {
