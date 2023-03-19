2390076b8c257497ad2b68c8ee7934ecb5e7a9c4
Mykola Kovshar <knikolay20031230@gmail.com>
2023-03-19 13:32:00 +0200

    Update BLL Services

---
+++ BLL/Services/ProducerService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.Repositories;
+using DAL.Tables;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ProducerService : IService<ProducerDTO>
+    {
+        private IMapper _mapper;
+        private ProducerRepository _repository;
+
+        public ProducerService(ProducerRepository repository)
+        {
+            _repository = repository;
+
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Producer, ProducerDTO>()
+                .ForMember(x => x.Id, x => x.MapFrom(producer => producer.Id))
+                .ForMember(x => x.ProducerName, x => x.MapFrom(producer => producer.ProducerName));
+            });
+
+            _mapper = new Mapper(config);
+        }
+
+        public IEnumerable<ProducerDTO> GetAll() => _mapper.Map<IEnumerable<ProducerDTO>>(_repository.GetAll());
+
+        public async Task<IEnumerable<ProducerDTO>> GetAllAsync() => await Task.Run(() => GetAll());
+
+        public ProducerDTO Get(int id) => _mapper.Map<ProducerDTO>(_repository.Get(id));
+
+        public void Delete(ProducerDTO producerDTO)
+        {
+            var producer = _repository.Get(producerDTO.Id);
+            _repository.Remove(producer);
+        }
+
+        public ProducerDTO Add(ProducerDTO producer)
+        {
+            Producer producerToAdd = new Producer
+            {
+                ProducerName = producer.ProducerName
+            };
+            _repository.AddUpdate(producerToAdd);
+            producer.Id = producerToAdd.Id;
+            return producer;
+        }
+
+        public void Update(ProducerDTO producer)
+        {
+            Producer producerToUpdate = _repository.Get(producer.Id);
+            producerToUpdate.ProducerName = producerToUpdate.ProducerName;
+            _repository.AddUpdate(producerToUpdate);
+        }
+    }
+}
---
+++ BLL/Services/RoleService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.Repositories;
+using DAL.Tables;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    internal class RoleService : IService<RoleDTO>
+    {
+        private RoleRepository _repository;
+        private IMapper _mapper;
+
+        public RoleService(RoleRepository repository)
+        {
+            _repository = repository;
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Role, RoleDTO>()
+                .ForMember(x => x.Id, x=> x.MapFrom(p => p.Id))
+                .ForMember(x => x.RoleName, x => x.MapFrom(p => p.RoleName));
+            });
+
+            _mapper = new Mapper(config);
+        }
+
+        public RoleDTO Add(RoleDTO roleDTO)
+        {
+            Role roletoAdd = new Role
+            {
+                RoleName = roleDTO.RoleName
+            };
+            _repository.AddUpdate(roletoAdd);
+            roleDTO.Id = roletoAdd.Id;
+            return roleDTO;
+        }
+
+        public void Delete(RoleDTO roleDTO)
+        {
+            Role role = _repository.Get(roleDTO.Id);
+            _repository.Remove(role);
+        }
+
+        public RoleDTO Get(int id) => _mapper.Map<RoleDTO>(_repository.Get(id));
+
+        public IEnumerable<RoleDTO> GetAll() => _mapper.Map<IEnumerable<RoleDTO>>(_repository.GetAll());
+
+        public async Task<IEnumerable<RoleDTO>> GetAllAsync() => await Task.Run(() => GetAll());
+
+        public void Update(RoleDTO roleDTO)
+        {
+            Role role = _repository.Get(roleDTO.Id);
+            role.RoleName = roleDTO.RoleName;
+            _repository.AddUpdate(role);
+        }
+    }
+}
