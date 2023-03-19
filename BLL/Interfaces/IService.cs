3e4abffb7e8b7073b154c3cea7f33145df4e515c
Mykola Kovshar <knikolay20031230@gmail.com>
2023-03-19 13:30:05 +0200

    Update BLL Interfaces

---
+++ BLL/Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IService<T> where T : IDTOEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        void Delete(T entity);
+        T Add(T entity);
+        void Update(T entity);
+    }
+}
