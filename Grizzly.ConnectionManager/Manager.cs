8156825e54c374e6de045c604dc1a4501ea0912c
Mykola Kovshar <knikolay20031230@gmail.com>
2023-04-07 18:21:26 +0300

    Add Connection manager that contains url & port

---
+++ Grizzly.ConnectionManager/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grizzly.ConnectionManager
+{
+    public static class Manager
+    {
+        public static string Url => "http://localhost:";
+        public static int Port => 8080;
+    }
+}
