a79e4ff2953ba545dd5e9a4b40ac61bf0c5609eb
Mykola Kovshar <knikolay20031230@gmail.com>
2023-04-12 10:44:48 +0300

    Configurated server to listening port & receiving request with text in its body

--- Grizzly.Server/Program.cs
+++ Grizzly.Server/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Grizzly.ConnectionManager;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +18,7 @@ namespace Grizzly.Server
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add($"{Manager.Url}{Manager.Port}/");
             listener.Start();
+            Console.WriteLine("Server started listeting port: " + Manager.Port);
 
             while (listener.IsListening)
             {
@@ -31,7 +34,15 @@ namespace Grizzly.Server
 
         static async Task GetRequest(HttpListenerContext context)
         {
-            Console.WriteLine("Request");
+            Console.WriteLine("Got request");
+
+            var str = context.Request.InputStream;
+            using (StreamReader ms = new StreamReader(str))
+            {
+                string str1 = ms.ReadToEnd();
+                Console.WriteLine(str1);
+            }
+
             var response = context.Response;
             
         }
