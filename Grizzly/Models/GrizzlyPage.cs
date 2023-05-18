729e81a835f069f65a55abe30329179dc857bbc2
Mykola Kovshar <knikolay20031230@gmail.com>
2023-05-18 15:16:27 +0300

    Update UI

---
+++ Grizzly/Models/GrizzlyPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Grizzly.Models
+{
+    public class GrizzlyPage : Page
+    {
+        public GrizzlyPage(GrizzlyPage child = null)
+        {
+            Child = child;
+        }
+
+        public GrizzlyPage Child { get; set; } = null;
+    }
+}
---
+++ Grizzly/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Grizzly.Pages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Grizzly.ViewModels
+{
+    public class MainViewModel
+    {
+        public bool ForwardArrowEnabled { get; set; } = false;
+        public bool BackwardArrowEnabled { get; set; } = false;
+        public Page CurrentPage { get; set; } = new DrugListPage();
+        
+        public ObservableCollection<string> UserMenuOptions = new ObservableCollection<string>();
+
+        public MainViewModel()
+        {
+            UserMenuOptions.Add("Log in");
+            UserMenuOptions.Add("Sign in");
+        }
+    }
+}
