c0c9840ec99bfb0d914099c04c7f39c4c766a191
Mykola Kovshar <knikolay20031230@gmail.com>
2023-06-18 20:18:50 +0300

    Rewrite Producers Controller to use HttpResponseMessage

--- Grizzly.Server/Controllers/ProducersController.cs
+++ Grizzly.Server/Controllers/ProducersController.cs
@@ -3,81 +3,132 @@ using BLL.Services;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 namespace Grizzly.Server.Controllers
 {
     public class ProducersController : ApiController
     {
         private ProducerService _service;
+        private HttpResponseMessage _message;
         public ProducersController(ProducerService service)
         {
             _service = service;
         }
 
         //GET: api/producers
-        public async Task<JObject> Get()
+        public async Task<HttpResponseMessage> Get()
         {
             var producers = await _service.GetAllAsync();
             JArray array = new JArray();
+            JObject result;
+
+            if (producers.Count() == 0)
+            {
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.NoContent);
+                string json = @"{
+                                    Count: 0,
+                                    Producers: []
+                                }";
+                result = JObject.Parse(json);
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+                return _message;
+            }
             try
             {
-                foreach (var producer in producers)
+                foreach(var producer in producers)
                 {
-                    array.Add(JToken.FromObject(producer));
+                    array.Add(producer);
                 }
-                JObject result = new JObject();
+                result = new JObject();
+                result["Count"] = array.Count;
                 result["Producers"] = array;
-                return result;
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.OK);
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
             }
             catch(Exception ex)
             {
-                string respone = @"{
-                                    Producers: []
-                                    }";
-                return JObject.FromObject(respone);
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, ex);
             }
-
+            return _message;
         }
 
         // GET: api/producers/5
-        public async Task<JObject> Get(int id)
+        public async Task<HttpResponseMessage> Get(int id)
+        {
+            JObject result;
+            try
+            {
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Found);
+                var producer = _service.Get(id);
+                if(producer == null)
+                {
+                    _message.StatusCode = System.Net.HttpStatusCode.NotFound;
+                    string json = @"{
+                                        Producer: null
+                                    }";
+                    result = JObject.Parse(json);
+                }
+                else
+                {
+                    result = JObject.FromObject(producer);
+                }
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
+        }
+        //POST api/prudcers
+        public async Task<HttpResponseMessage> Post([FromBody] JObject value)
         {
             try
             {
-                return await Task.Run(() => JObject.FromObject(_service.Get(id)));
+                ProducerDTO producer = JsonConvert.DeserializeObject<ProducerDTO>(value.ToString());
+                producer = _service.Add(producer);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Created);
+                JObject result = JObject.FromObject(producer);
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
             }
-            catch (Exception ex)
+            catch(Exception e)
             {
-                string response = @"
-                                    {{
-                                        Status Code: 404 Not Found,
-                                        Message: Producer with with id is not found
-                                    }}";
-                return JObject.FromObject(response);
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
             }
+            return _message;
         }
-        //POST api/prudcers
-        public async Task<JObject> Post([FromBody] JObject value)
+        public async Task<HttpResponseMessage> Put([FromBody] JObject value)
         {
-            ProducerDTO producer = JsonConvert.DeserializeObject<ProducerDTO>(value.ToString());
-            producer = _service.Add(producer); return JObject.FromObject(producer);
+            try
+            {
+                ProducerDTO producer = JsonConvert.DeserializeObject<ProducerDTO>(value.ToString());
+                _service.Update(producer);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Accepted);
+                _message.Content = new StringContent(JObject.FromObject(producer).ToString(), Encoding.UTF8, "application/json");
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
         }
-        public async Task<JObject> Put([FromBody] JObject value)
+        public async Task<HttpResponseMessage> Delete([FromBody] JObject value)
         {
-            ProducerDTO producer = JsonConvert.DeserializeObject<ProducerDTO>(value.ToString());
-            _service.Update(producer);
-            return JObject.FromObject(producer);
-        }
-        public async Task Delete([FromBody] JObject value)
-        {
-            ProducerDTO producer = JsonConvert.DeserializeObject<ProducerDTO>(value.ToString());
-            _service.Delete(producer);
+            try
+            {
+                ProducerDTO producer = JsonConvert.DeserializeObject<ProducerDTO>(value.ToString());
+                _service.Delete(producer);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
         }
     }
 }
