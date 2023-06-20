bfc20c906fd12653b207c36391943dbbd00ad95e
Mykola Kovshar <knikolay20031230@gmail.com>
2023-06-20 16:58:46 +0300

    Rewrite Login algorithm

--- Grizzly.Server/Controllers/LoginController.cs
+++ Grizzly.Server/Controllers/LoginController.cs
@@ -1,43 +1,49 @@
 ﻿using BLL.DTO;
-using Grizzly.ConnectionManager;
+using BLL.Services;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace Grizzly.Server.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController : ApiController
     {
-        private UsersController controller;
+        private HttpResponseMessage _message;
+        private UserService _service;
 
-        public LoginController(UsersController controller)
+        public LoginController(UserService service)
         {
-            this.controller = controller;
+            _service = service;
         }
 
-        //POST api/login
-        public async Task<JObject> Post([FromBody] JObject value)
+        public async Task<HttpResponseMessage> Login([FromBody]JObject value)
         {
-            var usersJson = await controller.Get();
-            var users = JsonConvert.DeserializeObject<List<UserDTO>>(usersJson.ToString());
-            var loggedUser = JsonConvert.DeserializeObject<UserDTO>(value.ToString());
-            UserDTO user = users.Find(x => x.Login == loggedUser.Login && x.Password == loggedUser.Password);
-            if (user != null)
+            JObject result;
+            try
             {
-                return JObject.FromObject(user);
+                var user = JsonConvert.DeserializeObject<UserDTO>(value.ToString());
+                var users = await _service.GetAllAsync();
+                if(users.Count() > 0)
+                {
+                    var userToFind = users.Where(x => x.Login == user.Login && x.Password == user.Password).First();
+                    if (userToFind != null)
+                    {
+                        result = JObject.FromObject(userToFind);
+                        _message = Request.CreateResponse(System.Net.HttpStatusCode.OK, Encoding.UTF8, "application/json");
+                    }
+                }
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.NotFound);
             }
-            string response = @"{
-                                Status code : 404 Not Found,
-                                Message :  Invalid login or password
-                                }";
-            return JObject.FromObject(response);
+            catch(Exception ex)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, ex);
+            }
+            return _message;
         }
     }
 }
