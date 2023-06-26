3703d2e1c8eab1fc895e9399253606d735eb61eb
Mykola Kovshar <knikolay20031230@gmail.com>
2023-06-26 11:30:28 +0300

    Rewrite IndicationController

--- Grizzly.Server/Controllers/IndicationsController.cs
+++ Grizzly.Server/Controllers/IndicationsController.cs
@@ -9,12 +9,15 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Text;
 
 namespace Grizzly.Server.Controllers
 {
-    public class IndicationsController : Controller
+    public class IndicationsController : ApiController
     {
         private DrugIndicationService _service;
+        private HttpResponseMessage _message;
 
         public IndicationsController(DrugIndicationService service)
         {
@@ -22,66 +25,119 @@ namespace Grizzly.Server.Controllers
         }
 
         //GET: api/indications
-        public async Task<JObject> Get()
+        public async Task<HttpResponseMessage> Get()
         {
-            var indications = await _service.GetAllAsync();
-
             JArray array = new JArray();
+            JObject result;
             try
             {
+                var indications = await _service.GetAllAsync();
+
+                if(indications.Count() == 0)
+                {
+                    _message = Request.CreateResponse(System.Net.HttpStatusCode.NoContent);
+                    string json = @"{
+                                    Count:0,
+                                    Indications: []
+                                }";
+                    result = JObject.Parse(json);
+                    _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+                    return _message;
+                }
+
                 foreach (var indication in indications)
                 {
                     array.Add(JToken.FromObject(indication));
                 }
-                JObject result = new JObject();
+                result = new JObject();
+                result["Count"] = array.Count;
                 result["Indications"] = array;
-                return result;
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.OK);
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
             }
             catch (Exception ex)
             {
-                string respone = @"{
-                                    Indications: []
-                                    }";
-                return JObject.FromObject(respone);
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, ex);
             }
+            return _message;
         }
 
         // GET: api/indications/5
-        public async Task<JObject> Get(int id)
+        public async Task<HttpResponseMessage> Get(int id)
+        {
+            JObject result;
+            try
+            {
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Found);
+                var indication = _service.Get(id);
+                if(indication == null)
+                {
+                    _message.StatusCode = System.Net.HttpStatusCode.NotFound;
+                    string json = @"{
+                                        Indication: null
+                                    }";
+                    result = JObject.Parse(json);
+                }
+                else
+                {
+                    result = JObject.FromObject(indication);
+                }
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+            }
+            catch(Exception ex)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, ex);
+            }
+            return _message;
+        }
+
+        public async Task<HttpResponseMessage> Post([FromBody] JObject value)
+        {
+            JObject result;
+            try
+            {
+                DrugIndicationDTO indication = JsonConvert.DeserializeObject<DrugIndicationDTO>(value.ToString());
+                indication = _service.Add(indication);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Created);
+                result = JObject.FromObject(indication);
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
+        }
+
+        public async Task<HttpResponseMessage> Put([FromBody] JObject value)
         {
             try
             {
-                return await Task.Run(() => JObject.FromObject(_service.Get(id)));
+                DrugIndicationDTO indication = JsonConvert.DeserializeObject<DrugIndicationDTO>(value.ToString());
+                _service.Update(indication);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Accepted);
+                _message.Content = new StringContent(value.ToString(), Encoding.UTF8, "application/json");
             }
-            catch (Exception ex)
+            catch(Exception e)
             {
-                string response = @"
-                                    {{
-                                        Status Code: 404 Not Found,
-                                        Message: Indication with with id is not found
-                                    }}";
-                return JObject.FromObject(response);
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
             }
+            return _message;
         }
 
-        public async Task<JObject> Post([FromBody] JObject value)
+        public async Task<HttpResponseMessage> Delete([FromBody] JObject value)
         {
-            DrugIndicationDTO indication = JsonConvert.DeserializeObject<DrugIndicationDTO>(value.ToString());
-            indication = _service.Add(indication);
-            return JObject.FromObject(indication);
-        }
-
-        public async Task<JObject> Put([FromBody] JObject value)
-        {
-            DrugIndicationDTO indication = JsonConvert.DeserializeObject<DrugIndicationDTO>(value.ToString());
-            _service.Update(indication);
-            return JObject.FromObject(indication);
-        }
-
-        public async Task Delete([FromBody] JObject value)
-        {
-            DrugIndicationDTO indication = JsonConvert.DeserializeObject<DrugIndicationDTO>(value.ToString());
-            _service.Delete(indication);
+            try
+            {
+                DrugIndicationDTO indication = JsonConvert.DeserializeObject<DrugIndicationDTO>(value.ToString());
+                _service.Delete(indication);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
         }
     }
 }
