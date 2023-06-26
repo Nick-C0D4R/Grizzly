a461375d47dbf4e31aee5eba3828c0558421005c
Mykola Kovshar <knikolay20031230@gmail.com>
2023-06-26 12:47:01 +0300

    Rewrite ContraindicationsController

--- Grizzly.Server/Controllers/ContraindicationsController.cs
+++ Grizzly.Server/Controllers/ContraindicationsController.cs
@@ -9,12 +9,15 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Text;
 
 namespace Grizzly.Server.Controllers
 {
-    public class ContraindicationsController : Controller
+    public class ContraindicationsController : ApiController
     {
         private ContraIndicationService _service;
+        private HttpResponseMessage _message;
 
         public ContraindicationsController(ContraIndicationService service)
         {
@@ -22,66 +25,118 @@ namespace Grizzly.Server.Controllers
         }
 
         //GET: api/contraindications
-        public async Task<JObject> Get()
+        public async Task<HttpResponseMessage> Get()
         {
-            var contraindications = await _service.GetAllAsync();
-
             JArray array = new JArray();
+            JObject result;
             try
             {
-                foreach (var contraindication in contraindications)
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.OK);
+                var contraIndications = await _service.GetAllAsync();
+                if(contraIndications.Count() == 0)
                 {
-                    array.Add(JToken.FromObject(contraindication));
-                }
-                JObject result = new JObject();
-                result["Contraindications"] = array;
-                return result;
-            }
-            catch (Exception ex)
-            {
-                string respone = @"{
-                                    Contraindications: []
+                    _message = Request.CreateResponse(System.Net.HttpStatusCode.NoContent);
+                    string json = @"{
+                                        Count: 0,
+                                        ContraIndications: []
                                     }";
-                return JObject.FromObject(respone);
+                    result = JObject.Parse(json);
+                }
+                else
+                {
+                    result = new JObject();
+                    foreach(var contraIndication in contraIndications)
+                    {
+                        array.Add(contraIndication);
+                    }
+                    result["Count"] = array.Count;
+                    result["ContraIndications"] = array;
+                }
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
             }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
         }
 
         // GET: api/contraindications/5
-        public async Task<JObject> Get(int id)
+        public async Task<HttpResponseMessage> Get(int id)
+        {
+            JObject result;
+            try
+            {
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Found);
+                var contraIndication = _service.Get(id);
+                if(contraIndication == null)
+                {
+                    _message.StatusCode = System.Net.HttpStatusCode.NotFound;
+                    string json = @"{
+                                        ContraIndication: null
+                                    }";
+                    result = JObject.Parse(json);
+                }
+                else
+                {
+                    result = JObject.FromObject(contraIndication);
+                }
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
+        }
+
+        public async Task<HttpResponseMessage> Post([FromBody] JObject value)
+        {
+            JObject result;
+            try
+            {
+                var contraIndication = JsonConvert.DeserializeObject<DrugContraIndicationDTO>(value.ToString());
+                contraIndication = _service.Add(contraIndication);
+                result = JObject.FromObject(contraIndication);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Created);
+                _message.Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
+        }
+
+        public async Task<HttpResponseMessage> Put([FromBody] JObject value)
         {
             try
             {
-                return await Task.Run(() => JObject.FromObject(_service.Get(id)));
+                DrugContraIndicationDTO contraIndication = JsonConvert.DeserializeObject<DrugContraIndicationDTO>(value.ToString());
+                _service.Update(contraIndication);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.Accepted);
+                _message.Content = new StringContent(value.ToString(), Encoding.UTF8, "application/json");
             }
-            catch (Exception ex)
+            catch(Exception e)
             {
-                string response = @"
-                                    {{
-                                        Status Code: 404 Not Found,
-                                        Message: Contraindication with with id is not found
-                                    }}";
-                return JObject.FromObject(response);
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
             }
+            return _message;
         }
 
-        public async Task<JObject> Post([FromBody] JObject value)
+        public async Task<HttpResponseMessage> Delete([FromBody] JObject value)
         {
-            DrugContraIndicationDTO contraindication = JsonConvert.DeserializeObject<DrugContraIndicationDTO>(value.ToString());
-            contraindication = _service.Add(contraindication);
-            return JObject.FromObject(contraindication);
-        }
-
-        public async Task<JObject> Put([FromBody] JObject value)
-        {
-            DrugContraIndicationDTO contraindication = JsonConvert.DeserializeObject<DrugContraIndicationDTO>(value.ToString());
-            _service.Update(contraindication);
-            return JObject.FromObject(contraindication);
-        }
-
-        public async Task Delete([FromBody] JObject value)
-        {
-            DrugContraIndicationDTO contraindication = JsonConvert.DeserializeObject<DrugContraIndicationDTO>(value.ToString());
-            _service.Delete(contraindication);
+            try
+            {
+                DrugContraIndicationDTO contraindication = JsonConvert.DeserializeObject<DrugContraIndicationDTO>(value.ToString());
+                _service.Delete(contraindication);
+                _message = Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch(Exception e)
+            {
+                _message = Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, e);
+            }
+            return _message;
         }
     }
 }
